7a10d39117ac28284233c9b78d5c93f0064075b6
emirmehmedovic <emir.m@live.com>
2025-04-07 14:08:58 +0200

    Remove output:export for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export', // Removed this line to allow dynamic routes for CMS admin
   eslint: {
     ignoreDuringBuilds: true,
   },
